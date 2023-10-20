e000140e9f0726f1fb2a440a012cec75ce83d7e7
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2023-10-20 18:55:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ global.creator = 'StoreValzy'// yourname
 global.MONGO_DB_URI = "mongodb+srv://storevalzy:vf2N96HXvOTL8uOf@cluster0.grmbwsg.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "storevalzy@gmail.com" //email
-global.email_password = "thqc_phxa_drdw_niqy" //application password email
+global.email_password = "thqcphxadrdwniqy" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
