d4a955b08fe1020ae96bf5ac74894dd8378b3d2b
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2023-10-20 19:57:54 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'StoreValzy'// yourname
-global.MONGO_DB_URI = "mongodb+srv://storevalzy:vf2N96HXvOTL8uOf@cluster0.grmbwsg.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://leopyth:h9TORXwECFrKvEOo@cluster0.wwqf20p.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "storevalzy@gmail.com" //email
 global.email_password = "thqcphxadrdwniqy" //application password email
