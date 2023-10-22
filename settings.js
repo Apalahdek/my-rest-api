2cb08a4297c9fc18d8efb41a9fb17d9a33c9f2d1
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2023-10-22 19:32:55 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'heheheheboys10'// yourname
 global.MONGO_DB_URI = "mongodb+srv://leotyps:0dU0W9YYBmvJh7Vb@cluster0.mljvn4h.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "heheheheboys10@gmail.com" //email
-global.email_password = "qbrnwclbxugntpcj" //application password email
+global.your_email = "storevalzy@gmail.com" //email
+global.email_password = "thqcphxadrdwniqy" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
